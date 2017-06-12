f2795d4b4689845a83a67d927090e2c9acc445ee
vakun <vforvakun@gmail.com>
2017-06-13 01:46:55 +0300

    new files

---
+++ TicketBookingApplication/Models/SearchViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TicketBookingApplication.Models
+{
+    public class SearchViewModel
+    {
+        [Required(ErrorMessage ="Откуда летим?"), Display(Name = "Город отправления")]
+        public int DepartureCityId { get; set; }
+
+        [Required(ErrorMessage = "Куда летим?"), Display(Name = "Город прибытия")]
+        public int ArrivalCityId { get; set; }
+
+        [Required(ErrorMessage = "Когда летим?"), Display(Name = "Дата отправки"), DataType(DataType.Date)]
+        public DateTime DepartureDate { get; set; }
+        public string OrderField { get; set; }
+        public string OrderDirection { get; set; }
+    }
+}
