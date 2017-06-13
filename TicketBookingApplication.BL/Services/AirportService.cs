5362debd41182d15513590be3a6ddbbbd385772a
vakun <vforvakun@gmail.com>
2017-06-13 07:45:44 +0300

    registration and login added

--- TicketBookingApplication.BL/Services/AirportService.cs
+++ TicketBookingApplication.BL/Services/AirportService.cs
@@ -33,7 +33,7 @@ namespace TicketBookingApplication.BL.Services
                               DepartureTime = flight.DepartureTime
                           };
 
-            return flights.ToList().Select(x=>new Flight
+            return flights.ToList().Select(x => new Flight
             {
                 Id = x.Id,
                 Airplane = x.Airplane,
@@ -42,6 +42,48 @@ namespace TicketBookingApplication.BL.Services
                 ArrivalCity = x.ArrivalCity,
                 DepartureTime = x.DepartureTime
             }).ToList();
+        }
+
+        public User LoginUser(string login, string password)
+        {
+            return _uow.UserRepository.AllEntities.SingleOrDefault(x => x.Login == login && x.Password == password);
+        }
+
+        public Profile CreateProfile(string name, string surname, int age, bool sex)
+        {
+            var profile = new Profile
+            {
+                Age = age,
+                Name = name,
+                Sex = sex,
+                Surname = surname
+            };
+            _uow.ProfileRepository.Add(profile);
+            _uow.Save();
+
+            return profile;
+        }
+
+        public User CreateUser(Profile profile, string login, string password, string email)
+        {
+            var user = _uow.UserRepository.AllEntities.SingleOrDefault(x => x.Login == login);
+
+            if (user == null)
+            {
+                user = new User
+                {
+                    Login = login,
+                    Email = email,
+                    RegistrationDate = DateTime.Now,
+                    Password = password,
+                    Profile = profile
+                };
+
+                _uow.UserRepository.Add(user);
+                _uow.Save();
+            }
+
+            return user;
         }
     }
 }
--- TicketBookingApplication.DAL/UnitOfWork.cs
+++ TicketBookingApplication.DAL/UnitOfWork.cs
@@ -14,5 +14,10 @@ namespace TicketBookingApplication.DAL
         public ProfileRepository ProfileRepository { get; } = new ProfileRepository(_context);
         public TicketRepository TicketRepository { get; } = new TicketRepository(_context);
         public UserRepository UserRepository { get; } = new UserRepository(_context);
+
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
     }
 }
